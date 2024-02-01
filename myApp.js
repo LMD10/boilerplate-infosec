104b131d9584467567a766b15d0d0fc8ae702e22
Liam Salazar-Endara <liam.salazar-endara@student.manchester.ac.uk>
2024-02-01 19:23:21 +0000

    Content Security Policy

--- myApp.js
+++ myApp.js
@@ -10,7 +10,13 @@ const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'", 'trusted-cdn.com'],
+  }
+})
+);
 
 
 
